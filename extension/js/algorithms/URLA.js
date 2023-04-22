deb3d2f3901b8c3d10ed49e72aa06112f9860fe4
tanyiqu <1953649096@qq.com>
2023-04-22 21:42:09 +0800

    1.2.1版 更新URL编码算法

--- extension/js/algorithms/URLA.js
+++ extension/js/algorithms/URLA.js
@@ -11,15 +11,40 @@ URLA.decode = (content) => {
 
 
 
+
 /**
  * URL编码  (All char)
  */
 const URLALLA = new AlgorithmInterface();
-URLALLA.encode = (content) => {
-    console.log(content);
-    console.log(content.length);
-    return encodeURIComponent(content) + ' all'
+
+
+function getAsciiHex(i) {
+    var asciiCode = i.charCodeAt(0);
+    var hexValue = asciiCode.toString(16).toUpperCase(); // 转换为大写字母的十六进制格式
+    return hexValue;
 }
+
+
+
+
+// 自定义编码算法
+function encode(content) {
+    result = '';
+
+    for (let i = 0; i < content.length; i++) {
+        if (encodeURIComponent(content[i]) === content[i]) {
+            result += ('%' + getAsciiHex(content[i]));
+        } else {
+            result += encodeURIComponent(content[i]);
+        }
+    }
+
+    return result;
+}
+
+
+
+URLALLA.encode = encode;
 URLALLA.decode = (content) => {
-    return decodeURIComponent(content) + ' all'
+    return decodeURIComponent(content)
 }
